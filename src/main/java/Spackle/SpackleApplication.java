51e8f06d0f468012b79bd5cc7c018f3fb755176b
김민호 <2022305011@skuniv.ac.kr>
2026-03-30 14:06:34 +0900

    :tada: Start: First Commit

---
+++ src/main/java/Spackle/SpackleApplication.java
@@ -0,0 +1,13 @@
+package Spackle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpackleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpackleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Spackle/SpackleApplicationTests.java
@@ -0,0 +1,13 @@
+package Spackle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpackleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
